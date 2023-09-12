1ed7646558537676ff7f10531c1e6e26d04e5242
humbertocarcamo <hcarcamovasquez@gmail.com>
2023-09-12 13:04:42 -0300

    icon pokemon-640.png

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+export const Footer = () => {
+
+}
+
+
+//Image by <a href="https://pixabay.com/users/hsaart-8633812/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4657023">Harisankar Sahoo</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4657023">Pixabay</a>
